2fe52d3cfdab90da8964418fb19552c086c173fd
tengf <tengf@qti.qualcomm.com>
2025-08-19 03:23:28 +0000

    docs: update packages version
    Signed-off-by: tengf <tengf@qti.qualcomm.com>

--- ai_vision/sample_depth_estimation/setup.py
+++ ai_vision/sample_depth_estimation/setup.py
@@ -10,7 +10,7 @@ package_name = 'sample_depth_estimation'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(where='.', exclude=['test']),
     package_dir={'': '.'},
     data_files=[
