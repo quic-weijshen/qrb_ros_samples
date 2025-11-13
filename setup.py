b6ad66a32e3e5c7f11386a4193a6eaff08a8a36f
fulaliu <fulaliu@qti.qualcomm.com>
2025-11-13 18:04:15 +0800

    fix: bug fix for resnet 101
    Add tensor check before publish ros message
    
    Signed-off-by: fulaliu <fulaliu@qti.qualcomm.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ package_name = 'sample_resnet101'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='1.0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name,
